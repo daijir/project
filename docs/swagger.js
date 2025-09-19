d6feea28582d3b149cbe9a6f51a0580e75a76f72
daijir <dazhilangxianggen@gmail.com>
2025-09-19 13:29:28 +0900

    changed url of render

--- docs/swagger.js
+++ docs/swagger.js
@@ -12,7 +12,7 @@ const options = {
     servers: [
       { url: "http://localhost:3000", description: "Development server" },
       {
-        url: "https://project-zejn.onrender.com",
+        url: "https://project-c38u.onrender.com",
         description: "Production server",
       },
     ],
